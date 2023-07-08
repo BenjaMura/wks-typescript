222fdbf3981d7d2979a1b63e932a5283e5016862
BenjaMura <benjaminmuratore1@gmail.com>
2023-07-08 12:05:12 -0300

    App established

---
+++ src/components/App.tsx
@@ -0,0 +1,31 @@
+import { connect } from "react-redux";
+import { User, fetchUsers } from "../redux/actions/actions";
+import { StoreState } from "../redux/reducers/reducer";
+
+interface AppProps {
+  users: User[];
+  fetchUsers(): any;
+}
+
+function App(props: AppProps) {
+  return (
+    <div>
+      <button onClick={props.fetchUsers}>FETCH USERS!</button>
+      {props.users.map((user: User) => {
+        return (
+          <div key={user.id}>
+            {user.id} {user.name}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+const mapStateToProps = (state: StoreState): { users: User[] } => {
+  return {
+    users: state.users,
+  };
+};
+
+export default connect(mapStateToProps, { fetchUsers })(App);
